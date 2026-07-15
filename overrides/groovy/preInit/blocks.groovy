35807b0098e9f7ddd99f01e997f7fc230a81962f
Johny709 <janjoseph.merida@gmail.com>
2026-07-15 10:11:33 -0400

    start Gregtech CEU port. replace crafttweaker with groovyscript

---
+++ overrides/groovy/preInit/blocks.groovy
@@ -0,0 +1,8 @@
+import classes.BlockTJBlock
+
+log.info('registering TJ blocks')
+content.registerBlock('awakenedcasing', new BlockTJBlock())
+content.registerBlock('chaoticcasing', new BlockTJBlock())
+content.registerBlock('draconiccasing', new BlockTJBlock())
+content.registerBlock('eternityblock', new BlockTJBlock())
+content.registerBlock('soulcasing', new BlockTJBlock())
---
+++ overrides/groovy/preInit/items.groovy
@@ -0,0 +1,9 @@
+
+log.info('registering TJ items')
+content.createItem('eternitycrystal').register()
+content.createItem('eternityingot').register()
+content.createItem('infinitycrystal').register()
+content.createItem('refinedchaoscrystal').register()
+content.createItem('refinedchaosshard').register()
+content.createItem('steammotor').register()
+content.createItem('steampiston').register()
