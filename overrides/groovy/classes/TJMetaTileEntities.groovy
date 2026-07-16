e4a6491686c34322dc914b2c7b158b119c360a3d
Johny709 <janjoseph.merida@gmail.com>
2026-07-16 09:26:49 -0400

    add Industrial Primitive Blast Furnace

--- overrides/groovy/classes/TJMetaTileEntities.groovy
+++ overrides/groovy/classes/TJMetaTileEntities.groovy
@@ -3,4 +3,5 @@ import gregtech.api.metatileentity.MetaTileEntity
 class TJMetaTileEntities {
     public static MetaTileEntity PRIMITIVE_ALLOY_SMELTER
     public static MetaTileEntity COKE_OVEN
+    public static MetaTileEntity INDUSTRIAL_PRIMITIVE_BLAST_FURNACE
 }
--- overrides/groovy/preInit/tileentities.groovy
+++ overrides/groovy/preInit/tileentities.groovy
@@ -1,4 +1,5 @@
 
+import classes.MetaTileEntityIndustrialPrimitiveBlastFurnace
 import classes.MetaTileEntityPrimitiveAlloySmelter
 import classes.MetaTileEntityTJCokeOven
 import classes.TJMetaTileEntities
@@ -9,6 +10,7 @@ event_manager.listen { MTEManager.MTERegistryEvent event ->
     log.info('Registering TJ Tile Entities')
     TJMetaTileEntities.PRIMITIVE_ALLOY_SMELTER = MetaTileEntities.registerMetaTileEntity(0, new MetaTileEntityPrimitiveAlloySmelter(resource(getPackId(), 'primitive_alloy_smelter')))
     TJMetaTileEntities.COKE_OVEN = MetaTileEntities.registerMetaTileEntity(1, new MetaTileEntityTJCokeOven(resource(getPackId(), 'coke_oven')))
+    TJMetaTileEntities.INDUSTRIAL_PRIMITIVE_BLAST_FURNACE = MetaTileEntities.registerMetaTileEntity(2, new MetaTileEntityIndustrialPrimitiveBlastFurnace(resource(getPackId(), "industrial_primitive_blast_furnace")))
 }
 
 
