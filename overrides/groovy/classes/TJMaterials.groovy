acac49fec529c254b94252a044a8a156c52a4288
Johny709 <janjoseph.merida@gmail.com>
2026-08-02 20:25:24 -0400

    add materials for starlight rocket fuel

--- overrides/groovy/classes/TJMaterials.groovy
+++ overrides/groovy/classes/TJMaterials.groovy
@@ -7,4 +7,11 @@ class TJMaterials {
     public static Material DraconicStemCell
     public static Material EternityPreparation
     public static Material Eternity
+    public static Material HMX
+    public static Material NaquadricFuelCatalyst
+    public static Material Octaazacubane
+    public static Material OctaazacubaneSolution
+    public static Material StarlightComplex
+    public static Material ExplosiveHydrazineMixture
+    public static Material StarlightRocketFuel
 }
